17814421ace606447087bbf1f80ef57968b24ff3
emon <hk.emon.007@gmail.com>
2018-12-28 19:25:26 +0600

    Successfully access trello

--- src/main/java/TESTCLASS.java
+++ src/main/java/TESTCLASS.java
@@ -1,14 +1,25 @@
+import com.julienvey.trello.Trello;
+import com.julienvey.trello.domain.Board;
+import com.julienvey.trello.domain.Member;
+import com.julienvey.trello.domain.TList;
+import com.julienvey.trello.impl.TrelloImpl;
+import com.julienvey.trello.impl.http.ApacheHttpClient;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class TESTCLASS {
     public static void main(String[] args) {
-        final int x =10;
-        class test{
-            public  void m2(){
-                System.out.println(x);
-            }
+        String trelloKey = "b02d5e6a4657d5b6f34d33511519780e";
+        String trelloAccessToken = "c143cef55f94982c7d17b78f7c178189b16b4c5033397cbcc1a2b1674b8336cd";
+        Trello trelloApi = new TrelloImpl(trelloKey, trelloAccessToken, new ApacheHttpClient());
+
+        Board board = trelloApi.getBoard("TrTJ1ya6");
+        System.out.println(board.getName());
+        List<Member> lists = board.fetchMembers();
+        for (Member list : lists) {
+//            list.getName();
+            System.out.println(list.getFullName());
         }
-        test test = new test();
-        test.m2();
     }
 }
